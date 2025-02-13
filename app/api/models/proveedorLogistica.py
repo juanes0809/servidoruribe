82bbdacd8c3ba2087795226bba1a95b25a147650
jsantiar <145516657+jsantiar@users.noreply.github.com>
2025-02-12 20:25:40 -0500

    Iniciando proyecto

---
+++ app/api/models/proveedorLogistica.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, Integer, String, Date
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
+
+# Llamar a la base de datos
+# Definir las tablas del API
+
+class Proveedor():
+    __tablename__='proveedores'
+    nombres=Column(String(50))
+    documento=Column(String(50))
+    
